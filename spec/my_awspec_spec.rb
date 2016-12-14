3b94e545fd3d1cf0106fbda3fd3362e6b32168e7
jcl@skoll <larsonj.home@gmail.com>
2016-12-14 11:24:14 -0500

    packer vagrant timeout; awspec template

--- spec/my_awspec_spec.rb
+++ spec/my_awspec_spec.rb
@@ -1,10 +1,19 @@
 require 'spec_helper'
 
-describe vpc('example VPC') do
-   its(:cidr_block) { should eq '192.168.0.0/16' }
+describe vpc('dockerSwarm') do
+   it {should exist}
+   it {should have_tag('Stack').value('Networking') }
+   it {should have_tag('Environment').value('dev') }
 end
-describe vpn_gateway('igw-07ee7e60') do
+
+describe subnet('dmz-useast1-az1') do
    it { should exist }
+   it { should have_tag('Environment').value('dev') }
+end
+
+describe vpc('example VPC') do
+   it { should exist }
+   its(:cidr_block) { should eq '192.168.0.0/16' }
 end
 describe security_group('sg-24a64842') do 
    it { should exist }
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,7 @@
 require 'awspec'
 Awsecrets.load(secrets_path: File.expand_path('./secrets.yml', File.dirname(__FILE__)))
+RSpec.configure do |config|
+  config.failure_color = :magenta
+  config.tty = true
+  config.color = true
+end
